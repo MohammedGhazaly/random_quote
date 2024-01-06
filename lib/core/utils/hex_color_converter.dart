2c510919e855f869027160e31045083274943d3e
MohammedGhazaly <warendragon67@gmail.com>
2024-01-06 05:17:19 +0200

    Created hex color converter

---
+++ lib/core/utils/hex_color_converter.dart
@@ -0,0 +1,14 @@
+import 'dart:ui';
+import 'package:flutter/material.dart';
+
+class HexColorConverter extends Color {
+  static int _getColorFromHex(String hexColor) {
+    hexColor = hexColor.toUpperCase().replaceAll("#", "");
+    if (hexColor.length == 6) {
+      hexColor = "FF$hexColor";
+    }
+    return int.parse(hexColor, radix: 16);
+  }
+
+  HexColorConverter(final String hexColor) : super(_getColorFromHex(hexColor));
+}
