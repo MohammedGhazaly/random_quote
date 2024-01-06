29e2cacb0c2ec42ead7bcf41d17843ef37554d12
MohammedGhazaly <warendragon67@gmail.com>
2024-01-06 10:19:59 +0200

    Finished adding images to app

--- lib/core/utils/app_colors.dart
+++ lib/core/utils/app_colors.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:quote_gen_clean_arc/core/utils/hex_color_converter.dart';
 
 class AppColors {
   static Color primaryColor = const Color(0xff202053);
