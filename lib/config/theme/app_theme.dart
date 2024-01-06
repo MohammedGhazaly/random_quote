8bb1c3b417fd41b1ea32ecd823f062d7583205d7
MohammedGhazaly <warendragon67@gmail.com>
2024-01-06 10:06:26 +0200

    Finished adding app font

--- lib/config/theme/app_theme.dart
+++ lib/config/theme/app_theme.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:quote_gen_clean_arc/core/utils/app_colors.dart';
+import 'package:quote_gen_clean_arc/core/utils/app_strings.dart';
 
 class AppTheme {
   static ThemeData mainTheme() {
     return ThemeData(
       useMaterial3: true,
       primaryColor: AppColors.primaryColor,
+      fontFamily: AppStrings.appFontFamily,
       hintColor: AppColors.hintColor,
       brightness: Brightness.light,
       scaffoldBackgroundColor: Colors.white,
--- lib/core/utils/app_strings.dart
+++ lib/core/utils/app_strings.dart
@@ -1,3 +1,4 @@
 class AppStrings {
   static const String appName = "Quotes App";
+  static const String appFontFamily = "Tajawal";
 }
