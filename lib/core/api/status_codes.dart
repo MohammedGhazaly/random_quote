e0b5d63491e3346f778380173e003fb46183d6bd
MohammedGhazaly <warendragon67@gmail.com>
2024-01-13 06:59:16 +0200

    Added status code class

---
+++ lib/core/api/status_codes.dart
@@ -0,0 +1,9 @@
+class StatusCode {
+  static const ok = 200;
+  static const badRequest = 400;
+  static const unauthorized = 401;
+  static const forbidden = 403;
+  static const notFound = 404;
+  static const conflict = 409;
+  static const internalServerError = 500;
+}
